7af131a8c89d69c4791eac0d1501889f3bd1a20e
David Heiberg <dhdavvie@gmail.com>
2016-04-20 19:31:15 +0100

    first push

---
+++ discordbot.js
@@ -0,0 +1,55 @@
+var Discord = require('discord.js');
+var feed = require("feed-read");
+
+var lastDevBlog = {
+	title : ""
+};
+var lastPatchNotes = {
+	title : ""
+};
+var seconds = 5; //timer
+var timer = seconds * 1000;
+var bot = new Discord.Client();
+var channel;
+var botOn = false;
+console.log("Starting Discord Bot");
+
+bot.login("ech0opulse@gmail.com", "botpassword");
+
+
+bot.on("message", function(message){
+	if(message.content === "a" && !botOn && message.author.username === "Echo Utrigas") {
+		botOn = true;
+		console.log(message.author);
+		
+		console.log("Bot being turned ON");
+		bot.sendMessage(channel, "Dev Blog/Patch Notes RSS Feed ON!", function(err) {if(err) throw err;});
+		setInterval(function(){ 
+			feed("https://newsfeed.eveonline.com/en-US/2/articles/page/1/20", function(err, devblogs){
+				if(err) throw err;
+				if(!(devblogs[0].title === lastDevBlog.title)) {
+					lastDevBlog = devblogs[0]; //set new last article
+					bot.sendMessage(message.channel, lastDevBlog.link, function(err){
+						if(err) console.log("error : "+channel.id+" " + err);
+					});
+				}
+			});
+			feed("https://newsfeed.eveonline.com/en-US/15/articles/page/1/5", function(err, patchnotes){
+				if(err) throw err;
+				if(!(patchnotes[0].title === lastPatchNotes.title)) {
+					lastPatchNotes = patchnotes[0]; //set new last article
+					bot.sendMessage(message.channel, lastPatchNotes.link, function(err){
+						if(err) console.log("error : "+channel.id+" " + err);
+					});
+				}
+			});
+		}, timer);
+	}
+	if(message.content === "b" && botOn && message.author.username === "Echo Utrigas") {
+		botOn = false;
+		console.log("Bot being turned OFF");
+		bot.sendMessage(channel, "Bot turning off!", function(err){if(err) throw err;});
+	}
+
+	
+});
