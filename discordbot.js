2fd5d95f3eaef82c3d9516ad6cf35adcda2f2eac
David H <dhdavvie@gmail.com>
2016-04-29 18:53:23 -0700

    Added timestamp to console output

--- discordbot.js
+++ discordbot.js
@@ -1,5 +1,8 @@
 var Discord = require('discord.js');
 var feed = require("feed-read");
+var moment = require('moment');
+
+
 
 var lastDevBlog = {
 	title : ""
@@ -15,23 +18,27 @@ var timer = seconds * 1000;
 var bot = new Discord.Client();
 var channel;
 var botOn = false;
-console.log("Starting Discord Bot");
+console.log( getTime() + " - Starting Discord Bot");
 
 bot.login("ech0opulse@gmail.com", "botpassword");
 
+function getTime() {
+	var time = "["+moment().format('D/M/YY - h:mm:ss a')+"]";
+	return time;
+}
 
 bot.on("message", function(message){
 	if(message.content === "!BotOn" && !botOn && message.author.username === "Echo Utrigas") {
 		botOn = true;
 		
-		console.log("Bot being turned ON");
+		console.log(getTime() + " - Bot being turned ON");
 		bot.sendMessage(channel, "Dev Blog/Patch Notes RSS Feed ON!", function(err) {if(err) throw err;});
 		setInterval(function(){ 
 			feed("https://newsfeed.eveonline.com/en-US/2/articles/page/1/20", function(err, devblogs){
 				if(err) throw err;
 				if(!(devblogs[0].title === lastDevBlog.title)) {
 					lastDevBlog = devblogs[0]; //set new last article
-					console.log("New dev blog: "+lastDevBlog.link);
+					console.log(getTime() + " - New dev blog: "+lastDevBlog.link);
 					bot.sendMessage(message.channel, lastDevBlog.link, function(err){
 						if(err) console.log("error : "+channel.id+" " + err);
 					});
@@ -41,7 +48,7 @@ bot.on("message", function(message){
 				if(err) throw err;
 				if(!(evenews[0].title === lastEveNews.title)) {
 					lastEveNews = evenews[0]; //set new last article
-					console.log("New Eve news: "+lastEveNews.link);
+					console.log(getTime() + " - New Eve news: "+lastEveNews.link);
 					bot.sendMessage(message.channel, lastEveNews.link, function(err){
 						if(err) console.log("error : "+channel.id+" " + err);
 					});
@@ -51,7 +58,7 @@ bot.on("message", function(message){
 				if(err) throw err;
 				if(!(patchnotes[0].title === lastPatchNotes.title)) {
 					lastPatchNotes = patchnotes[0]; //set new last article
-					console.log("New patch notes: "+lastPatchNotes.link);
+					console.log(getTime() + " - New patch notes: "+lastPatchNotes.link);
 					bot.sendMessage(message.channel, lastPatchNotes.link, function(err){
 						if(err) console.log("error : "+channel.id+" " + err);
 					});
@@ -61,7 +68,7 @@ bot.on("message", function(message){
 	}
 	if(message.content === "!BotOff" && botOn && message.author.username === "Echo Utrigas") {
 		botOn = false;
-		console.log("Bot being turned OFF");
+		console.log(getTime() + " - Bot being turned OFF");
 		bot.sendMessage(channel, "Bot turning off!", function(err){if(err) throw err;});
 	}
 
