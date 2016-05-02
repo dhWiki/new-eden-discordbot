0a21a08471a71c84cbd900766bd309bf8546f791
dhdavvie <dhdavvie@gmail.com>
2016-05-02 14:13:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {};
 
 config.discord = {};
-config.youtube_api_key = 'AIzaSyDmPSi-LlJxvyIJw-LDGYE_ndaCQJjy_CE';
+config.youtube_api_key = 'YoutubeAPIKey';
 
-config.discord.username = 'ech0opulse@gmail.com';
+config.discord.username = 'email@gmail.com';
 config.discord.password = 'botpassword';
 
 config.admins = ['Echo Utrigas'];
